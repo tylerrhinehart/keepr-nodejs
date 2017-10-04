f4ed5f0f61bd3f9ba3b5799b869b65f79576b887
Tyler Rhinehart <rhinehart22@gmail.com>
2017-10-04 13:08:09 -0600

    singlekeepview has menu

--- wwwsrc/src/components/SingleKeepView.vue
+++ wwwsrc/src/components/SingleKeepView.vue
@@ -2,18 +2,29 @@
     <v-card>
         <v-card-media :src="activeKeep.url" height="400px">
         </v-card-media>
-        <v-card-title primary-title>
-            <div>
-                <h3 class="headline mb-0">{{activeKeep.title}}</h3>
-                <div>{{activeKeep.description}}</div>
-            </div>
+        <v-card-title class="blue white--text">
+            <span class="headline">{{activeKeep.title}}</span>
+            <v-spacer></v-spacer>
+            <v-menu bottom right>
+                <v-btn icon slot="activator" dark>
+                    <v-icon>more_vert</v-icon>
+                </v-btn>
+                <v-list>
+                    <v-list-tile v-for="item in items" :key="item.title" @click="item.function">
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
         </v-card-title>
-        <v-card-actions>
+        <v-card-text>
+            <span>{{activeKeep.description}}</span>
+        </v-card-text>
+        <!-- <v-card-actions>
             <v-btn flat class="orange--text">Share</v-btn>
             <v-btn flat class="orange--text add-keep" @click="selectKeep">Keep</v-btn>
             <p>{{activeKeep.adds}}</p>
             <v-icon>remove_red_eye</v-icon>
-        </v-card-actions>
+        </v-card-actions> -->
     </v-card>
 </template>
 
@@ -22,12 +33,24 @@
         name: 'SingleKeepView',
         data() {
             return {
-
+                items: [
+                    {
+                        title: 'Keep',
+                        function: this.selectKeep
+                    },
+                    {
+                        title: 'Share',
+                        function: ''
+                    }
+                ]
             }
         },
         methods: {
             selectKeep() {
                 this.$store.dispatch('showBottomVaultsBar')
+            },
+            deleteKeep() {
+                this.$store.dispatch('deleteKeep', this.keep._id)
             }
         },
         computed: {
@@ -38,6 +61,13 @@
         mounted() {
             this.$store.dispatch('findKeep', this.$route.params.keepId)
             this.$store.dispatch('incrementKeep', this.$route.params.keepId)
+            if (this.$store.state.user._id == this.activeKeep.creatorId) {
+                var remove = {
+                    title: 'Remove',
+                    function: this.deleteKeep
+                }
+                this.items.push(remove)
+            }
         }
     }
 
