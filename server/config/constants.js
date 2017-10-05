debf828057b6e6ffe177bc32ff6d211daca922f0
Tyler Rhinehart <rhinehart22@gmail.com>
2017-10-05 09:04:42 -0600

    removed unnecessary models/custom-routes, initial push to heroku

--- server/config/constants.js
+++ server/config/constants.js
@@ -7,45 +7,20 @@ const actions = {
 }
 
 const models = {
-  board: {
-    name: 'Board',
-    endpoint: 'boards',
-    preventDefaultApi: true,
-    useCustomRoutes: true
-  },
-  list: {
-    name: 'List',
-    endpoint: 'lists',
-    preventDefaultApi: true,
-    useCustomRoutes: true
-  },
   user: {
     name: 'User',
     endpoint: 'users',
     preventDefaultApi: true,
     useCustomRoutes: true
   },
-  task: {
-    name: 'Task',
-    endpoint: 'tasks',
-    preventDefaultApi: true,
-    useCustomRoutes: true
-  },
-  comment: {
-    name: 'Comment',
-    endpoint: 'comments',
-    // useCustomRoutes:true,
-  },
   vault: {
     name: 'Vault',
     endpoint: 'vaults',
-    // preventDefaultApi: true,
     useCustomRoutes: true
   },
   keep: {
     name: 'Keep',
     endpoint: 'keeps',
-    // preventDefaultApi: true,
     useCustomRoutes: true
   },
 }
