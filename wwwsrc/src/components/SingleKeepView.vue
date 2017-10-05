a27cb385597c0ef9342a4ca7dbc0b687e1d0211b
Tyler Rhinehart <rhinehart22@gmail.com>
2017-10-05 09:45:54 -0600

    adjusted styling on singlekeepview

--- wwwsrc/src/components/SingleKeepView.vue
+++ wwwsrc/src/components/SingleKeepView.vue
@@ -1,31 +1,38 @@
 <template>
-    <v-card>
-        <v-card-media :src="activeKeep.url" height="400px">
-        </v-card-media>
-        <v-card-title class="blue white--text">
-            <span class="headline">{{activeKeep.title}}</span>
-            <v-spacer></v-spacer>
-            <v-menu bottom right>
-                <v-btn icon slot="activator" dark>
-                    <v-icon>more_vert</v-icon>
-                </v-btn>
-                <v-list>
-                    <v-list-tile v-for="item in items" :key="item.title" @click="item.function">
-                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    </v-list-tile>
-                </v-list>
-            </v-menu>
-        </v-card-title>
-        <v-card-text>
-            <span>{{activeKeep.description}}</span>
-        </v-card-text>
-        <!-- <v-card-actions>
+    <v-layout>
+        <v-flex offset-xs1 xs10>
+            <v-card>
+                <v-card-media :src="activeKeep.url" height="75vh">
+                </v-card-media>
+                <v-card-title class="blue white--text">
+                    <span class="headline">{{activeKeep.title}}</span>
+                    <v-spacer></v-spacer>
+                    <span>{{activeKeep.adds}}</span>
+                    <v-icon>remove_red_eye</v-icon>
+                    <v-spacer></v-spacer>
+                    <v-menu bottom right>
+                        <v-btn icon slot="activator" dark>
+                            <v-icon>more_vert</v-icon>
+                        </v-btn>
+                        <v-list>
+                            <v-list-tile v-for="item in items" :key="item.title" @click="item.function">
+                                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                            </v-list-tile>
+                        </v-list>
+                    </v-menu>
+                </v-card-title>
+                <v-card-text>
+                    <span>{{activeKeep.description}}</span>
+                </v-card-text>
+                <!-- <v-card-actions>
             <v-btn flat class="orange--text">Share</v-btn>
             <v-btn flat class="orange--text add-keep" @click="selectKeep">Keep</v-btn>
             <p>{{activeKeep.adds}}</p>
             <v-icon>remove_red_eye</v-icon>
         </v-card-actions> -->
-    </v-card>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
