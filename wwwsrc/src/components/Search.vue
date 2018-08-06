23566b2f4f94cbd2b95728fb8086ceb3610330db
tyler <tyler.rhinehart@portonefive.com>
2018-08-06 14:15:41 -0600

    primitive search

---
+++ wwwsrc/src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="site-search">
+        <v-text-field v-model="searchValue"></v-text-field>
+            <v-icon @click="search">search</v-icon>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "search",
+  data() {
+    return {
+      searchValue: ""
+    };
+  },
+  methods: {
+    search() {
+      this.$store.dispatch("search", { value: this.searchValue });
+      console.log(this.searchValue);
+    }
+  }
+};
+</script>
+
+<style>
+#site-search {
+  display: flex;
+}
+</style>
